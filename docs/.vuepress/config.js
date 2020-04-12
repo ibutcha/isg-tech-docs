e580ed1394ac64013a61a15abcbd9bc570467805
Butch Santos <cusantos@globe.com.ph>
2020-04-12 15:55:36 +0800

    Add configuration with navigation configs.

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports = {
+    title: 'SDC Technical Documentation',
+    description: '',
+    themeConfig: {
+        nav: [
+            {
+                text: 'Databases',
+                ariaLabel: 'Databases menu',
+                link: '/'
+            },
+            {
+                text: 'Web Servers',
+                link: '/'
+            }
+        ]
+    }
+};
