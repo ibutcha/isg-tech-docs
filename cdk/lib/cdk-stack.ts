bd26ad6b369c7a24bd97161e683c7b8956a6e95a
Butch Santos <cusantos@globe.com.ph>
2020-04-13 22:19:39 +0800

    Add CDK app for S3 Configuration.

---
+++ cdk/lib/cdk-stack.ts
@@ -0,0 +1,17 @@
+import * as cdk from '@aws-cdk/core';
+import * as s3 from '@aws-cdk/aws-s3';
+
+export class CdkStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    // The code that defines your stack goes here
+    new s3.Bucket(this, 'sdc-docs-v2', {
+      bucketName: 'sdc-docs-v2',
+      versioned: false,
+      publicReadAccess: true,
+      websiteIndexDocument: 'index.html',
+      websiteErrorDocument: 'index.html'
+    });
+  }
+}
